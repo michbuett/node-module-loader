88365d4f8200f05fe382c94ce7d4feaca9971f6e
Michael Büttner <michael.buettner@flyeralarm.com>
2015-08-28 12:32:52 +0200

    Simple minimizer for normalizer function

--- src/LoaderBuilder.js
+++ src/LoaderBuilder.js
@@ -17,7 +17,7 @@ module.exports = {
         var moduleList = analyzer.collectScripts(dependencyMap);
         var loaderScript = template(fs.readFileSync(loaderTpl, 'utf8'))({
             sourcePath: cfg.sourcePath || '',
-            normalize: normalize.toString(),
+            normalize: normalize.toString().replace(/([\)\(\{\}\,\;])\s+/g, '$1'),
             moduleList: JSON.stringify(moduleList),
             dependencyMap: JSON.stringify(dependencyMap),
         });
